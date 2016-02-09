82cc611e42e87ab94d1163825b12b8da86a355df
Gert van Tonder <gvantonder@ampco.com.au>
2016-02-09 11:58:08 +1100

    Moved Foundation JS + CSS into foundationize vendor package.

--- FoundationAsset.php
+++ FoundationAsset.php
@@ -11,22 +11,30 @@ use yii\web\AssetBundle;
 
 class FoundationAsset extends AssetBundle 
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    //public $basePath = '@webroot';
+//    public $basePath = null; // see construct further below
+//    public $baseUrl = '@web';
+//    public $sourcePath = null;
+    
+    public $sourcePath = '@vendor/foundationize/yii2-foundation';
     
     public $css = [
-        'https://cdn.jsdelivr.net/foundation/6.1.1/foundation.min.css', // The core Foundation CSS
+        // The core Foundation CSS
+        //'https://cdn.jsdelivr.net/foundation/6.1.1/foundation.min.css', 
+        // For CSS autocompleting, we'll use direct files instead of CDN
+        'css/foundation.min.css'
     ];
     // JS awesomeness: http://www.jsdelivr.com/projects/foundation
     public $js = [
-        'https://cdn.jsdelivr.net/foundation/6.1.1/foundation.min.js',   // The core Foundation JS
+        //'https://cdn.jsdelivr.net/foundation/6.1.1/foundation.min.js',   // The core Foundation JS
         // We will use Yii2's built-in front and backend validation instead of abide
         //'https://cdn.jsdelivr.net/foundation/6.1.1/js/foundation.abide.js',
-        'js/foundationize.js'
+        'js/foundation.min.js',
+        'js/foundationize.js',        
     ];
     // Depends on jQuery
     public $depends = [
         'yii\web\JqueryAsset'
     ]; 
-    
+       
 }
